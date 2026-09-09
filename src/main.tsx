c08f7f3bc2b02858500821411a4cea7239a3a79c
Fatih <130773931+fatiharslan21@users.noreply.github.com>
2026-09-09 23:43:30 +0300

    style: load mobile dashboard v1.8 overrides

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { NearbyDataProvider } from './context/NearbyDataContext'
 import './styles.css'
+import './mobile-dashboard-v18.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
