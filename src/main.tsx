a0e3c98c8fad4020b99e6027cc65a8e212315f2c
Fatih <130773931+fatiharslan21@users.noreply.github.com>
2026-09-10 00:10:27 +0300

    feat: wrap app with graceful error recovery

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { AppErrorBoundary } from './components/AppErrorBoundary'
 import { NearbyDataProvider } from './context/NearbyDataContext'
 import './styles.css'
 import './mobile-dashboard-v18.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <NearbyDataProvider>
-      <App />
-    </NearbyDataProvider>
+    <AppErrorBoundary>
+      <NearbyDataProvider>
+        <App />
+      </NearbyDataProvider>
+    </AppErrorBoundary>
   </React.StrictMode>,
 )
