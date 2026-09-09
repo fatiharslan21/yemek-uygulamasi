9ba36cf4fd71b7a4f698196acfed3ef382a07a70
Fatih <130773931+fatiharslan21@users.noreply.github.com>
2026-09-10 00:34:30 +0300

    style: apply product-facing cleanup rules

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { AppErrorBoundary } from './components/AppErrorBoundary'
 import { NearbyDataProvider } from './context/NearbyDataContext'
 import './styles.css'
 import './mobile-dashboard-v18.css'
+import './product-cleanup.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
