bcbed8a2fba02dd8e7a8bd7eec7af7f8b0d5a190
Fatih <130773931+fatiharslan21@users.noreply.github.com>
2026-09-09 23:02:28 +0300

    feat: provide nearby data across app

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { NearbyDataProvider } from './context/NearbyDataContext'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <NearbyDataProvider>
+      <App />
+    </NearbyDataProvider>
   </React.StrictMode>,
 )
