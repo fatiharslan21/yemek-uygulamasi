56fb2e6e34c511611b1aab9dcc4f187c0cd2891f
Fatih <130773931+fatiharslan21@users.noreply.github.com>
2026-09-10 09:47:54 +0300

    feat: merge expanded recipe families into catalog

--- src/data/recipeCatalog.ts
+++ src/data/recipeCatalog.ts
@@ -1,5 +1,6 @@
 import { BASE_INGREDIENTS, BASE_RECIPES } from './baseRecipeCatalog'
 import { EXTRA_INGREDIENTS, EXTRA_RECIPES } from './extraRecipes'
+import { EXPANDED_RECIPES } from './expandedRecipes'
 import type { IngredientDefinition, Recipe } from '../types'
 
 export const INGREDIENTS: IngredientDefinition[] = [...BASE_INGREDIENTS, ...EXTRA_INGREDIENTS]
@@ -8,7 +9,7 @@ export const INGREDIENT_BY_ID = Object.fromEntries(
   INGREDIENTS.map((item) => [item.id, item]),
 ) as Record<string, IngredientDefinition>
 
-export const RECIPE_CATALOG: Recipe[] = [...BASE_RECIPES, ...EXTRA_RECIPES]
+export const RECIPE_CATALOG: Recipe[] = [...BASE_RECIPES, ...EXTRA_RECIPES, ...EXPANDED_RECIPES]
 
 export const RECIPE_LIBRARY_STATS = {
   recipes: RECIPE_CATALOG.length,
