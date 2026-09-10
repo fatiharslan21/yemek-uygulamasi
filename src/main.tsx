6a967a634818f0b9e03031c02675c8eab0850b2d
Fatih <130773931+fatiharslan21@users.noreply.github.com>
2026-09-10 07:56:02 +0300

    feat: initialize native auth deep links

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import { AppErrorBoundary } from './components/AppErrorBoundary'
 import { NearbyDataProvider } from './context/NearbyDataContext'
+import { initializeNativeAuthLinks } from './services/nativeAuthLinks'
 import './styles.css'
 import './mobile-dashboard-v18.css'
 import './product-cleanup.css'
 import './accessibility.css'
+
+void initializeNativeAuthLinks()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
