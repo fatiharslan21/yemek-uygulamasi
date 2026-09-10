ef9e7ec1e5ee6c7b47ff676c5c782b6a89a86267
Fatih <130773931+fatiharslan21@users.noreply.github.com>
2026-09-10 10:01:04 +0300

    feat: bootstrap native persistence before rendering

--- src/main.tsx
+++ src/main.tsx
@@ -4,22 +4,29 @@ import App from './App'
 import { AppErrorBoundary } from './components/AppErrorBoundary'
 import { NearbyDataProvider } from './context/NearbyDataContext'
 import { initializeNativeAuthLinks } from './services/nativeAuthLinks'
+import { initializeNativePersistence } from './services/nativePersistence'
 import './styles.css'
 import './mobile-dashboard-v18.css'
 import './product-cleanup.css'
 import './accessibility.css'
+import './mobile-polish-v2.css'
 
-void initializeNativeAuthLinks()
+async function bootstrap() {
+  await initializeNativePersistence()
+  void initializeNativeAuthLinks()
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <a className="skip-link" href="#lokma-app-content">Ana içeriğe geç</a>
-    <div id="lokma-app-content" tabIndex={-1}>
-      <AppErrorBoundary>
-        <NearbyDataProvider>
-          <App />
-        </NearbyDataProvider>
-      </AppErrorBoundary>
-    </div>
-  </React.StrictMode>,
-)
+  ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+      <a className="skip-link" href="#lokma-app-content">Ana içeriğe geç</a>
+      <div id="lokma-app-content" tabIndex={-1}>
+        <AppErrorBoundary>
+          <NearbyDataProvider>
+            <App />
+          </NearbyDataProvider>
+        </AppErrorBoundary>
+      </div>
+    </React.StrictMode>,
+  )
+}
+
+void bootstrap()
